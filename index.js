7e408ce91484d216939fd075c50258c8b213cd84
janani <janani8836@gmail.com>
2021-06-22 16:45:05 +0530

    first commit

--- index.js
+++ index.js
@@ -27,8 +27,6 @@ async function findMoviesWithComments(){
             '$exists': true
           }
         }
-      }, {
-        '$limit': 30
       }
     ]
     const aggCursor =  Movie.aggregate(pipeline);
